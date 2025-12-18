c69e9fb3dd20b76900cf43b2d9459d7734d3640b
sivasaikum <sivasaikumarallampati@gmail.com>
2025-12-18 08:52:09 +0530

    terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,29 +1,29 @@
 resource "aws_instance" "nginx" {
-    ami = "ami-09c813fb71547fc4f"
-    vpc_security_group_ids = [aws_security_group.allow_tls.id]
-    instance_type = "t3.micro"
-    tags = {
-        Name = "nginx"
-        purpose= "Terrafrom"
-    }
+  ami                    = "ami-09c813fb71547fc4f"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  instance_type          = "t3.micro"
+  tags = {
+    Name    = "nginx"
+    purpose = "Terrafrom"
+  }
 }
 
 resource "aws_security_group" "allow_tls" {
-    name = "Allow-all"
-    description = "Allow TLS inbound traffic and all outbound traffic"
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    tags = {
-        Name = "allow-all"
-    }
+  name        = "Allow-all"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow-all"
+  }
 }
--- ec2/provider.tf
+++ ec2/provider.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 6.0"
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
   }
-  
+
 }
 
 provider "aws" {
